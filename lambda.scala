a7152513ff773f9c7d4c5756e23186ba84e5a605
Jan Soendermann <jan.soendermann@googlemail.com>
2014-02-17 00:12:55 +0000

    Parsing, free and bound variables and substitution implemented

---
+++ lambda.scala
@@ -0,0 +1,68 @@
+import scala.util.parsing.combinator._
+
+// ######## AST #########
+abstract class LTerm
+
+case class LVariable(c: Char) extends LTerm { override def toString() = { c.toString() }}
+case class LAbstraction(v: LVariable, m: LTerm) extends LTerm { override def toString() = { "λ" + v + "." + m.toString() }}
+case class LApplication(m: LTerm, n: LTerm) extends LTerm { override def toString() = { "(" + m.toString() + " " + n.toString() +")" }}
+
+
+// ######## Parser #########
+object LambdaParsers extends JavaTokenParsers {
+  def term: Parser[LTerm] = variable | abstraction | application
+
+  def variable: Parser[LVariable] = """[a-z]""".r ^^ {s => (LVariable(s.head))}
+
+  def abstraction: Parser[LAbstraction] = "L"~>variable~"."~term ^^ {
+    case v~"."~m => LAbstraction(v, m)
+  }
+
+  def application: Parser[LApplication] = "("~>term~term<~")" ^^ {
+    case m~n => LApplication(m, n)
+  }
+
+  def parseExpr(s: String) = parse(term, s).get
+}
+
+
+// ######### functions ##########
+def free_vars(t: LTerm) : Set[LVariable] = t match {
+  case LVariable(c) => Set(LVariable(c))
+  case LAbstraction(v, m) => free_vars(m) -- Set(v)
+  case LApplication(m, n) => free_vars(m) ++ free_vars(n)
+}
+
+def bound_vars(t: LTerm) : Set[LVariable] = t match {
+  case LVariable(_) => Set()
+  case LAbstraction(v, m) => Set(v) ++ bound_vars(m)
+  case LApplication(m, n) => bound_vars(m) ++ bound_vars(n)
+}
+
+def fresh() : LVariable = {
+  LVariable('f')
+}
+
+def substitute(m: LTerm, x: LVariable, n: LTerm) : LTerm = m match {
+  case LVariable(_) => {
+    if (m == x)
+      n
+    else
+      m
+  }
+  case LAbstraction(v, p) => {
+    if (v == x)
+      m
+    else if (!free_vars(p).contains(x) || !free_vars(n).contains(v))
+      LAbstraction(v, substitute(p, x, n))
+    else if (free_vars(p).contains(x) && free_vars(n).contains(v)) {
+      val z = fresh()
+      substitute(LAbstraction(z, substitute(p, v, z)), x, n)
+    } else {
+      throw new IllegalArgumentException()
+    }
+  }
+  case LApplication(p, q) => LApplication(substitute(p, x, n), substitute(q, x, n))
+}
+
+println(LambdaParsers.parseExpr("Lx.(x x)"))
